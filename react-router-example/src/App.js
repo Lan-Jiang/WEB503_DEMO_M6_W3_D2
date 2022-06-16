97c8068dd447797dd6c4a97f67761cab38a79737
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-06-15 18:35:10 -0700

    WEB503 DEMO M6W3D2 initiate

---
+++ react-router-example/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return <Router> {(
+    
+  )
+}</Router>;
+}
+
+export default App;
